598a7d2b6dc977482a306a799e4b9c3d757e9c6d
Nat Tan <tresarpnat27@gmail.com>
2024-06-04 17:35:59 +0700

    design button and alert box

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,41 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "error") {
+    return (
+      <button
+        css={css`
+          background-color: #de5753;
+          padding-bottom: 16px;
+          padding-top: 16px;
+          height: 50px;
+          width: 200px;
+          border-radius: 7px;
+          margin-right: 10px;
+        `}
+      >
+        Large
+      </button>
+    );
+  } else if (props.type === "success") {
+    return (
+      <button
+        css={css`
+          background-color: #26b795;
+          padding-bottom: 16px;
+          padding-top: 16px;
+          height: 50px;
+          width: 200px;
+          border-radius: 7px;
+          margin-left: 10px;
+        `}
+      >
+        Large
+      </button>
+    );
+  }
+}
+
+export default Button;
