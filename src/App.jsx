57b22db3b491cfe6433e1cd50a5b708b4b717564
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-04 16:06:45 +0700

    feat(App.jsx, Button.jsx): add Component Button by CSS Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import "./App.css";
+import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <div>
+          <h2>Primary</h2>
+          <Button color="primary" />
+        </div>
+        <div>
+          <h2>Secondary</h2>
+          <Button color="secondary" />
+        </div>
       </div>
       <hr />
       <div className="alert-components-section">
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,36 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props) {
+  let bgColor;
+  if (props.color === 'primary') {
+    bgColor = '#074EE8';
+  } else if (props.color === 'secondary') {
+    bgColor = '#07A4E8';
+  }
+  return (
+    <button
+      css={css`
+        padding: 32px 100px;
+        margin-left: 100px;
+        background-color: ${bgColor};
+        font-size: 24px;
+        border: none;
+        border-radius: 5px;
+        text-align: center;
+        &:hover {
+          color: white;
+        }
+      `}
+    >
+      {/* {props.color} */}
+      Large
+    </button>
+  );
+}
+
+export default Button;
+
+// background-color: #4caf50;
+// background-color: ${props.color === 'primary' ? '#074EE8 ' : '#07A4E8'};
